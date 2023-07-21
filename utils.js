b57538d38f0d5ed3adf6e46ffbda6155b834eb35
tangping <1959152133@qq.com>
2023-07-21 14:19:48 +0800

    fix

--- utils.js
+++ utils.js
@@ -4,21 +4,25 @@ const prettierConfig = require('./prettierrc')
 
 module.exports = {
   defineEslintConfig(extendsConfig = {}) {
-    const { prettier } = extendsConfig
+    const { prettier, prettierVue } = extendsConfig
     delete extendsConfig.prettier
     const _extends = _.concat(baseConfig.extends, extendsConfig.extends || [])
     const config = {
-      ...baseConfig,
-      ...extendsConfig,
+      ..._.merge({}, baseConfig, extendsConfig),
       extends: _extends
     }
     if (prettier) {
+      const _prettier = typeof prettier === 'boolean' ? ['error', prettierConfig] : prettier
       config.extends = _.concat(config.extends, ['plugin:prettier/recommended'])
+      if (prettierVue) {
+        //
+      }
       _.merge(config.rules, {
-        'prettier/prettier': ['error', prettierConfig]
+        'prettier/prettier': _prettier
       })
     }
 
     return config
   }
 }
+
